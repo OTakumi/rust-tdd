785a8d6971aa63265981f6731934e06ce96abc93
OTakumi <takumi.o.0410@gmail.com>
2022-09-26 23:53:17 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+// ToDo List
+// TODO: $5 + 10 CHF = $10
+// TODO: Moneyの丸目処理どうする？
+
+#[derive(Debug)]
+mod money {
+    pub struct Doller {
+        pub amount: u32,
+    }
+
+    impl Doller {
+        pub fn new(amount: u32) -> Doller {
+            Doller { amount: amount }
+        }
+
+        pub fn times(&mut self, multiplier: u32) -> Doller {
+            Doller { amount: self.amount * multiplier }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiplication() {
+        // n倍した合計値を確認する
+        let mut five = money::Doller::new(5);
+        let mut product = five.times(2);
+        assert_eq!(10, product.amount);
+
+        product = five.times(3);
+        assert_eq!(15, product.amount);
+    }
+}
+
