cd1988fae2ecb9347606d7500e025e614018ef79
OTakumi <takumi.o.0410@gmail.com>
2022-10-02 00:08:37 +0900

    timesメソッドがDollerを返すことが分かりやすいテストに変更した。
    - timesメソッドがDollerを返すことが分かりやすいテストに変更した。
    - amountをprivateに変更した。

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 // ToDo List
-// TODO: $5 + 10 CHF = $10
+// TODO: $5 + 10 CHF = $10(レートが2:1の場合)
 // TODO: Moneyの丸目処理どうする？
+// DONE: amountをprivateにする。
 // DONE: equals()
 // TODO: hashCode()
+// TODO: nullとの等価性比較
+// TODO: 他オブジェクトとの等価性比較
 
 #[derive(Debug)]
 pub struct Doller {
-    pub amount: u32,
+    amount: u32,
 }
 
 impl Doller {
     /// Create a Doller instance.
     pub fn new(amount: u32) -> Doller {
-        Doller { amount }
+        Self { amount }
     }
 
     /// Calculate multiplication with the entered multiples.
@@ -23,7 +26,7 @@ impl Doller {
     }
 
     /// Check the equivalent.
-    pub fn equals(&self, other: &Doller) -> bool {
+    pub fn equals(&self, other: Doller) -> bool {
         self.amount == other.amount
     }
 }
@@ -36,16 +39,13 @@ mod tests {
     fn test_multiplication() {
         // n倍した合計値を確認する
         let mut five = Doller::new(5);
-        let mut product = five.times(2);
-        assert_eq!(10, product.amount);
-
-        product = five.times(3);
-        assert_eq!(15, product.amount);
+        assert!(Doller::new(10).equals(five.times(2)));
+        assert!(Doller::new(15).equals(five.times(3)));
     }
 
     #[test]
     fn test_equality() {
-        assert!(Doller::new(5).equals(&Doller::new(5)));
-        assert!(Doller::new(5).equals(&Doller::new(6)));
+        assert!(Doller::new(5).equals(Doller::new(5)));
+        assert!(!Doller::new(5).equals(Doller::new(6)));
     }
 }
